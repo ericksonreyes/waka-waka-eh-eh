87d17ba1e732e0e81a73b1ddfae731f83767b614
Erickson Reyes <ercbluemonday@yahoo.com>
2023-06-01 17:25:27 +0800

    Make tests pass.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Services\ServiceFeeCalculator\ServiceCalculator;
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
@@ -9,6 +11,22 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements Context
 {
+
+    /**
+     * @var float
+     */
+    private $distanceTravelled = 0.00;
+
+    /**
+     * @var float
+     */
+    private $cashCollection = 0.00;
+
+    /**
+     * @var ServiceCalculator
+     */
+    private $serviceCalculator;
+
     /**
      * Initializes context.
      *
@@ -19,4 +37,58 @@ class FeatureContext implements Context
     public function __construct()
     {
     }
+
+    /**
+     * @Given the distance travelled by the delivery rider is :arg1 kilometers
+     */
+    public function theDistanceTravelledByTheDeliveryRiderIsKilometers(float $distanceTravelled)
+    {
+        $this->distanceTravelled = $distanceTravelled;
+    }
+
+    /**
+     * @Given the rider collected an amount of :arg1 from the consignee
+     */
+    public function theRiderCollectedAnAmountOfFromTheConsignee(float $cashCollection)
+    {
+        $this->cashCollection = $cashCollection;
+    }
+
+    /**
+     * @When service fee is computed
+     */
+    public function serviceFeeIsComputed()
+    {
+        $distance = $this->distanceTravelled;
+        $unitOfMeasurement = 'km';
+
+        $this->serviceCalculator = new ServiceCalculator($distance, $unitOfMeasurement);
+    }
+
+    /**
+     * @Then the service fee should be :arg1
+     */
+    public function theServiceFeeShouldBe(float $expectedServiceFee)
+    {
+        assert(
+            $this->serviceCalculator->serviceFee() ===  $expectedServiceFee,
+            'Expected service fee of ' . $expectedServiceFee . ' was not met. ' .
+            'The actual service fee computed is ' . $this->serviceCalculator->serviceFee()
+        );
+    }
+
+    /**
+     * @Then our expected revenue is :arg1
+     */
+    public function ourExpectedRevenueIs(float $expectedRevenue)
+    {
+        $actualRevenue = $this->cashCollection - $this->serviceCalculator->serviceFee();
+
+        assert(
+            $actualRevenue ===  $expectedRevenue,
+            'Expected revenue of ' . $expectedRevenue . ' was not met. ' .
+            'The actual revenue computed is ' . $actualRevenue
+        );
+    }
+
 }
--- features/service-calculator.feature
+++ features/service-calculator.feature
@@ -4,12 +4,14 @@ Feature: Service Calculator
   So that we know how much we owe the 3pl partner and our revenue
 
   Scenario Outline: Items are delivered by the third party logistics partner
-    Given the distance travelled by the delivery rider is <distance_traveled> kilometers
-    And the rider collected an amount of <cash_collection> from the consignee
+    Given the distance travelled by the delivery rider is <Distance Travelled> kilometers
+    And the rider collected an amount of <GGX Collection> from the consignee
     When service fee is computed
-    Then the service fee should be <service_fee>
-    And our expected revenue is <expected_revenue>
+    Then the service fee should be <Panda Go Service Fee>
+    And our expected revenue is <QuadX Revenue>
 
     Examples:
-      | distance_traveled | cash_collection | service_fee | expected_revenue |
-      | 4.1               | 88              | 69          | 19               |
+      | Distance Travelled | Panda Go Service Fee | GGX Collection | QuadX Revenue |
+      | 4.1                | 69                   | 88             | 19            |
+      | 0.00001            | 45                   | 88             | 43            |
+      | 6.000021           | 81                   | 88             | 7             |
